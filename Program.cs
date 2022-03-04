cf3349c8d1d1b04cf52e9baf17acac5b81be5400
sagarjaid23 <99254738+sagarjaid23@users.noreply.github.com>
2022-03-04 16:45:18 +0530

    Assignment Day 6

--- Program.cs
+++ Program.cs
@@ -1,29 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Diagnostics;
-using System.Threading;
 
-namespace Stopwatchdemo
+namespace Decimal_to_binary
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Stopwatch stopWatch = new Stopwatch();
-            stopWatch.Start();
-            Thread.Sleep(10000);
-            stopWatch.Stop();
-            
-            TimeSpan ts = stopWatch.Elapsed;
+            int n;
+            string bin = "";
 
-             string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
-                ts.Hours, ts.Minutes, ts.Seconds,
-                ts.Milliseconds / 10);
-            Console.WriteLine("RunTime " + elapsedTime);
+            Console.WriteLine("Enter Any Number");
+            n= int.Parse(Console.ReadLine());
 
+            for (; n !=0; n=n/2)
+            {
+                bin = n % 2 + bin;
+            }
+            Console.WriteLine("The Binary Format:" +bin);
         }
     }
 }

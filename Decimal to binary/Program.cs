4e905e96bb6a343cd24ef2371d70a4c214f13e97
sagarjaid23 <sagarjaid23@gmail.com>
2022-03-04 16:32:14 +0530

    Assignment Day 6

---
+++ Decimal to binary/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Decimal_to_binary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            string bin = "";
+
+            Console.WriteLine("Enter Any Number");
+            n= int.Parse(Console.ReadLine());
+
+            for (; n !=0; n=n/2)
+            {
+                bin = n % 2 + bin;
+            }
+            Console.WriteLine("The Binary Format:" +bin);
+        }
+    }
+}
